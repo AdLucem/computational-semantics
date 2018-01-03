6257a6029cf70e2349529dc4a80a5c8f5f1c83c9
AdLucem <ghosalatreyee@gmail.com>
2018-01-03 14:30:57 +0530

    finished chap.4. started chap.5

---
+++ cswfp/src/PredLogic.hs
@@ -0,0 +1,2 @@
+-- A module to implement predicate logic in Haskell
+
---
+++ cswfp/src/modelChecker.hs
@@ -0,0 +1,10 @@
+
+module MCWPL where
+
+import Data.List
+import FSynF
+import Model
+
+-- predicate logical formula
+type LF = Formula Term
+
